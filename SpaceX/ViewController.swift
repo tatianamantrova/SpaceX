81a832ae84524d99b9b12d337b910991f5b94ca5
Tatiana Mantrova <tatiana.mantrova@gmail.com>
2023-02-10 11:46:23 +0300

    Initial Commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Tatiana Mantrova on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
